7e5ce90b5fac181f9af5a79f4bae51a2f6d4feaa
tom-s <thom.schell@gmail.com>
2018-02-08 22:05:48 +0100

    working !

---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  context: __dirname + "/src",
+  entry: "./index",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new CleanWebpackPlugin(['dist'], {
+      exclude:  ['index.html'],
+    }),
+    new HtmlWebpackPlugin({
+      title: '#balancetonPOC',
+      template: 'index.html'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
