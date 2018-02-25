04e4d380d73c4d91742682134cccb7d05046e459
tom-s <thom.schell@gmail.com>
2018-02-25 13:54:25 +0100

    not bad

--- src/index.js
+++ src/index.js
@@ -23,21 +23,12 @@ class demo_opt {
   }
 }
 
-const inPolygon = (point, vs) => {
-  // ray-casting algorithm based on
-  // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
-  const x = point[0], y = point[1]
-  let inside = false
-  for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
-    var xi = vs[i][0], yi = vs[i][1]
-    var xj = vs[j][0], yj = vs[j][1]
-
-    var intersect = ((yi > y) != (yj > y))
-        && (x < (xj - xi) * (y - yi) / (yj - yi) + xi)
-    if (intersect) inside = !inside
-  }
-
-  return inside
+const isPointInPoly = (poly, pt) => {
+  for(var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
+      ((poly[i].y <= pt.y && pt.y < poly[j].y) || (poly[j].y <= pt.y && pt.y < poly[i].y))
+      && (pt.x < (poly[j].x - poly[i].x) * (pt.y - poly[i].y) / (poly[j].y - poly[i].y) + poly[i].x)
+      && (c = !c);
+  return c;
 }
 
 
